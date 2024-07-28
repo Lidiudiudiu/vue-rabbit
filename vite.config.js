56ec5586cc05c0fa37e8ea2abd0e629286e9d936
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-28 17:56:50 +0800

    element-plus定制主题色

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,8 +22,14 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+          // directives: true,
+          // version: "2.1.5",
+        }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
