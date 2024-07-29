da854cdb42bda3b0167947973681da9cf7bb54d6
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-29 14:26:17 +0800

    分类接口数据用pinia管理分发

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,13 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFix from './components/LayoutFix.vue';
+import { onMounted } from 'vue';
+import { useCategoryStore } from '@/stores/category';
+const categoryStore = useCategoryStore();
+
+onMounted(() => {
+    categoryStore.getCategory();
+})
 </script>
 
 <template>
