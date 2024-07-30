a52229aeabb0b647d594f034553073cb7d31777b
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-30 08:29:25 +0800

    组件封装 插槽和props传参

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,11 @@ import homeProduct from './components/HomeProduct.vue'
     </div>
     <HomeNew />
     <HomeHot />
-    <homeProduct />
+    <HomeProduct />
+    <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
+        插槽1
+    </HomePanel>
+    <HomePanel title="热门推荐" subtitle="热门推荐 好多商品">
+        插槽2
+    </HomePanel>
 </template>
