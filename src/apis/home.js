348f116e3cd567cf61009e20083994862dfcd903
Lidiudiudiu <08223241@cumt.edu.cn>
2024-07-30 09:04:27 +0800

    新鲜好物组件

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,9 @@ export function getBannerApi() {
         url: '/home/banner'
     })
 }
+
+export function getNewsApi() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
+
 </script>
 
 <template>
@@ -15,10 +15,4 @@ import HomePanel from './components/HomePanel.vue'
     <HomeNew />
     <HomeHot />
     <HomeProduct />
-    <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
-        插槽1
-    </HomePanel>
-    <HomePanel title="热门推荐" subtitle="热门推荐 好多商品">
-        插槽2
-    </HomePanel>
 </template>
