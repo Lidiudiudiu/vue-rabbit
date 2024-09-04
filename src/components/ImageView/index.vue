81e3bd0e7d7da8d1df62f8373a779dbaded0e2ad
Lidiudiudiu <08223241@cumt.edu.cn>
2024-09-04 20:10:44 +0800

    通过props传图片

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -39,17 +39,22 @@ const mouseEnterFn = (index) => {
     currentIndex.value = index
 }
 
-
+defineProps({
+    imageList: {
+        type: Array,
+        default: () => []
+    }
+})
 
 
 // 图片列表
-const imageList = [
-    "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-    "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-    "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-    "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-    "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+// const imageList = [
+//     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
+//     "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
+//     "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
+//     "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
+//     "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
+// ]
 </script>
 
 
@@ -75,7 +80,7 @@ const imageList = [
                 backgroundPositionX: `${positionX}px`,
                 backgroundPositionY: `${positionY}px`,
             },
-        ]"></div>
+        ]" v-show="!isOutside"></div>
     </div>
 </template>
 
