90c65d56335b69311ec07bc230bc5101b941f092
Lidiudiudiu <08223241@cumt.edu.cn>
2024-09-04 18:24:38 +0800

    阴影层跟随鼠标效果

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+const target = ref(null)
+
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+
+const left = ref(0);
+const top = ref(0);
+
+watch([elementX, elementY], () => {
+    // 有效范围内控制滑块距离
+    // 横向
+    if (isOutside.value) return;
+
+    if (elementX.value > 100 && elementX.value < 300) {
+        left.value = elementX.value - 100
+    }
+    // 纵向
+    if (elementY.value > 100 && elementY.value < 300) {
+        top.value = elementY.value - 100
+    }
+
+    // 处理边界
+    if (elementX.value > 300) { left.value = 200 }
+    if (elementX.value < 100) { left.value = 0 }
+
+    if (elementY.value > 300) { top.value = 200 }
+    if (elementY.value < 100) { top.value = 0 }
+})
 const currentIndex = ref(0)
 const mouseEnterFn = (index) => {
     currentIndex.value = index
@@ -21,7 +49,7 @@ const imageList = [
         <div class="middle" ref="target">
             <img :src="imageList[currentIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
